5783990ec35b8ee6af6d2e7028de5ca9ddf4307c
Еременко Аркадий Дмитриевич <eremenkoad@DOM201.LOCAL>
2024-10-05 10:04:48 +0700

    initial commit

--- src/common/Paths.ts
+++ src/common/Paths.ts
@@ -2,14 +2,20 @@
  * Express router paths go here.
  */
 
-
 export default {
-  Base: '/api',
+  Base: "/api",
   Users: {
-    Base: '/users',
-    Get: '/all',
-    Add: '/add',
-    Update: '/update',
-    Delete: '/delete/:id',
+    Base: "/users",
+    Get: "/all",
+    Add: "/add",
+    Update: "/update",
+    Delete: "/delete/:id",
+  },
+  Examples: {
+    Base: "/example",
+    Get: "/example/all",
+    Add: "/example/add",
+    Update: "/example/update",
+    Delete: "/example/delete/:id",
   },
 } as const;
--- src/repos/MockOrm.ts
+++ src/repos/MockOrm.ts
@@ -1,12 +1,10 @@
-
-
 import jsonfile from 'jsonfile';
 
 import { IUser } from '@src/models/User';
+import { ExampleType } from '@src/models/Example';
 
 
 // **** Variables **** //
-
 const DB_FILE_NAME = 'database.json';
 
 
@@ -14,6 +12,7 @@ const DB_FILE_NAME = 'database.json';
 
 interface IDb {
   users: IUser[];
+  examples: ExampleType[];
 }
 
 
@@ -22,8 +21,9 @@ interface IDb {
 /**
  * Fetch the json from the file.
  */
-function openDb(): Promise<IDb> {
-  return jsonfile.readFile(__dirname + '/' + DB_FILE_NAME) as Promise<IDb>;
+function openDb(jsonFile = DB_FILE_NAME): Promise<IDb> {
+  console.log(jsonfile.readFile(__dirname + '/' + jsonFile) as Promise<IDb>)
+  return jsonfile.readFile(__dirname + '/' + jsonFile) as Promise<IDb>;
 }
 
 /**
